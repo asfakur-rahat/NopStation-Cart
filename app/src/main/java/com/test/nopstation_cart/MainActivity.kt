334c018017050d8852ab8d71602e0c2a323e3409
Md Asfakur Rahat <asfakur.rahat@brainstation-23.com>
2024-05-15 20:09:25 +0600

    Add Salmon Fish Section on Homepage

--- app/src/main/java/com/test/nopstation_cart/MainActivity.kt
+++ app/src/main/java/com/test/nopstation_cart/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.test.nopstation_cart
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
