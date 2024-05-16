fc597a064ad196ea80b3da317a397157ffd1668f
Md Asfakur Rahat <asfakur.rahat@brainstation-23.com>
2024-05-16 17:46:27 +0600

    Add Login page UI

--- app/src/main/java/com/test/nopstation_cart/models/OurCategoryItem.kt
+++ app/src/main/java/com/test/nopstation_cart/models/OurCategoryItem.kt
@@ -3,5 +3,6 @@ package com.test.nopstation_cart.models
 data class OurCategoryItem(
     val id: Int,
     val categoryName: String,
-    val categoryImage: Int
+    val categoryImage: Int,
+    val categoryList: MutableList<ProductItem> = mutableListOf()
 )
--- app/src/main/java/com/test/nopstation_cart/screens/auth/LoginFragment.kt
+++ app/src/main/java/com/test/nopstation_cart/screens/auth/LoginFragment.kt
@@ -1,24 +1,22 @@
 package com.test.nopstation_cart.screens.auth
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import com.test.nopstation_cart.R
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
