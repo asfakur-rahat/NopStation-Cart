1a2b949364f604cfc488b6dafa012ff1ee6f2026
Md Asfakur Rahat <asfakur.rahat@brainstation-23.com>
2024-05-16 12:41:40 +0600

    Add Bottom Navbar to homepage

--- app/src/main/java/com/test/nopstation_cart/MainActivity.kt
+++ app/src/main/java/com/test/nopstation_cart/MainActivity.kt
@@ -3,11 +3,36 @@ package com.test.nopstation_cart
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.test.nopstation_cart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainNavbar.setOnItemSelectedListener { navitem ->
+            when(navitem.itemId){
+                R.id.home -> {
+                    true
+                }
+                R.id.category -> {
+                    true
+                }
+                R.id.search -> {
+                    true
+                }
+                R.id.account -> {
+                    true
+                }
+                R.id.more -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
